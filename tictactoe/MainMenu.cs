fa7ef1a3a3b44c460d957160e911cf15e557b0bc
Kenji Mende <kenjimende7@gmail.com>
2023-10-15 14:50:36 +0800

    Added forms and buttons to access forms

--- tictactoe/MainMenu.cs
+++ tictactoe/MainMenu.cs
@@ -1,13 +1,18 @@
 namespace tictactoe
 {//Poopy
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
